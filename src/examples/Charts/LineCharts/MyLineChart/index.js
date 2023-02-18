a83c4c2ec69133439ffcfb095445a90b968e754f
hovanthien26 <thien.hovan26@hcmut.edu.com>
2023-02-18 22:37:30 +0700

    Thu_7_18/2

---
+++ src/examples/Charts/LineCharts/MyLineChart/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+// import { Chart as ChartJS } from "chart.js/auto";
+
+function LineChart() {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }
+        ]
+      };
+    return <div><Line data={data} /></div>
+
+}
+export default LineChart;
--- src/layouts/tables/data/myDataLineChart.js
+++ src/layouts/tables/data/myDataLineChart.js
@@ -13,20 +13,35 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "App";
-
-// Material Dashboard 2 React Context Provider
-import { MaterialUIControllerProvider } from "context";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <MaterialUIControllerProvider>
-      <App />
-    </MaterialUIControllerProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-
+export const UserData = [
+  {
+    id: 1,
+    time: 2016,
+    value: 80000,
+    // userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
